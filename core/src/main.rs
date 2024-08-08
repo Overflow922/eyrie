2b634e83cd2e38fc82f356daa204406e48f05c56
Yury <genius922@yandex.ru>
2024-08-09 01:49:16 +0300

    temp commit

--- core/src/main.rs
+++ core/src/main.rs
@@ -19,16 +19,50 @@ async fn hello(rq: Request<Incoming>) -> Result<Response<Full<Bytes>>, String> {
     let (_first, _second) = tokio::join!(
         async {
             let cl = Client::builder(TokioExecutor::new()).build_http();
-            cl.request(wrap.to_rq(&bytes, "http://localhost:3001".parse::<Uri>().unwrap()))
+            cl.request(wrap.to_rq(&bytes, to_uri("http://localhost:3001")))
                 .await
         },
         async {
             let cl = Client::builder(TokioExecutor::new()).build_http();
-            cl.request(wrap.to_rq(&bytes, "http://localhost:3002".parse::<Uri>().unwrap()))
+            cl.request(wrap.to_rq(&bytes, to_uri("http://localhost:3002")))
                 .await
         }
     );
+
+    // check_result(_first, _second)
     Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+}
+
+// fn check_result(
+//     first: Result<Response<Incoming>, Error>,
+//     second: Result<Response<Incoming>, Error>,
+// ) -> Result<Response<Full<Bytes>>, String> {
+//     match first {
+//         Ok(rs) => match second {
+//             Ok(rs2) => {
+//                 let b_rs = rs.boxed();
+//                 let b_rs2 = rs2.boxed();
+//                 compare_rs(b_rs, b_rs2);
+//                 Ok(b_rs)
+//             }
+//             Err(e) => {
+//                 eprint!("error {}", e);
+//                 Ok(rs)
+//             }
+//         },
+//         Err(e) => Err(e.to_string()),
+//     }
+// }
+
+// fn compare_rs(
+//     boxed_1: http_body_util::combinators::BoxBody<Bytes, hyper::Error>,
+//     boxed_2: http_body_util::combinators::BoxBody<Bytes, hyper::Error>,
+// ) {
+//     boxed_1
+// }
+
+fn to_uri(uri: &str) -> Uri {
+    uri.parse::<Uri>().unwrap()
 }
 
 #[tokio::main]
