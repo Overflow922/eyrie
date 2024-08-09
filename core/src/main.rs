ebbac337b1d8025edfe7fa0fcf7c078ecdc10f65
Yury <genius922@yandex.ru>
2024-08-09 23:43:03 +0300

    add main compare logic

--- core/src/main.rs
+++ core/src/main.rs
@@ -6,60 +6,68 @@ use hyper::body::{Bytes, Incoming};
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
-use hyper_util::client::legacy::Client;
+use hyper_util::client::legacy::{Client, Error};
 use hyper_util::rt::{TokioExecutor, TokioIo};
-use prelude::RqWrapper;
 use tokio::net::TcpListener;
 
 mod prelude;
 
 async fn hello(rq: Request<Incoming>) -> Result<Response<Full<Bytes>>, String> {
-    let wrap = RqWrapper::new(&rq);
-    let bytes = rq.collect().await.expect("error").to_bytes();
+    let (h, b) = rq.into_parts();
+    let bytes = b.collect().await.expect("msg").to_bytes(); // can we avoid copying bytes?
     let (_first, _second) = tokio::join!(
         async {
             let cl = Client::builder(TokioExecutor::new()).build_http();
-            cl.request(wrap.to_rq(&bytes, to_uri("http://localhost:3001")))
+            let mut h1 = h.clone();
+            h1.uri = to_uri("http://localhost:3001");
+            cl.request(Request::from_parts(h1, Full::new(bytes.clone())))
                 .await
         },
         async {
             let cl = Client::builder(TokioExecutor::new()).build_http();
-            cl.request(wrap.to_rq(&bytes, to_uri("http://localhost:3002")))
+            let mut h1 = h.clone();
+            h1.uri = to_uri("http://localhost:3002");
+            cl.request(Request::from_parts(h1, Full::new(bytes.clone())))
                 .await
         }
     );
 
-    // check_result(_first, _second)
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+    check_result(_first, _second).await
 }
 
-// fn check_result(
-//     first: Result<Response<Incoming>, Error>,
-//     second: Result<Response<Incoming>, Error>,
-// ) -> Result<Response<Full<Bytes>>, String> {
-//     match first {
-//         Ok(rs) => match second {
-//             Ok(rs2) => {
-//                 let b_rs = rs.boxed();
-//                 let b_rs2 = rs2.boxed();
-//                 compare_rs(b_rs, b_rs2);
-//                 Ok(b_rs)
-//             }
-//             Err(e) => {
-//                 eprint!("error {}", e);
-//                 Ok(rs)
-//             }
-//         },
-//         Err(e) => Err(e.to_string()),
-//     }
-// }
+async fn check_result(
+    first: Result<Response<Incoming>, Error>,
+    second: Result<Response<Incoming>, Error>,
+) -> Result<Response<Full<Bytes>>, String> {
+    match first {
+        Ok(rs) => match second {
+            Ok(rs2) => choose_rs(rs, rs2).await,
+            Err(e) => {
+                eprint!("error {}", e);
+                let (parts, body) = rs.into_parts();
+                let body = body.collect().await.expect("msg").to_bytes();
+                Ok(Response::from_parts(parts, Full::new(body)))
+            }
+        },
+        Err(e) => Err(e.to_string()),
+    }
+}
 
-// fn compare_rs(
-//     boxed_1: http_body_util::combinators::BoxBody<Bytes, hyper::Error>,
-//     boxed_2: http_body_util::combinators::BoxBody<Bytes, hyper::Error>,
-// ) {
-//     boxed_1
-// }
+async fn choose_rs(
+    left: Response<Incoming>,
+    right: Response<Incoming>,
+) -> Result<Response<Full<Bytes>>, String> {
+    let (parts_l, body_l) = left.into_parts();
+    let (_, body_r) = right.into_parts();
+    let left = body_l.collect().await.expect("msg").to_bytes();
+    let right = body_r.collect().await.expect("msg").to_bytes();
+    if left == right {
+        Ok(Response::from_parts(parts_l, Full::new(left.clone())))
+    } else {
+        eprint!("rq2 differs from rq1!");
+        Ok(Response::from_parts(parts_l, Full::new(left.clone())))
+    }
+}
 
 fn to_uri(uri: &str) -> Uri {
     uri.parse::<Uri>().unwrap()
